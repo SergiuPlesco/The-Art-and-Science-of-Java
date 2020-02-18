5692607db99d48e87c8d6b441c456401c65dd982
Sergiu Plesco <plescosergiu@gmail.com>
2020-02-18 15:58:19 +0000

    Update RadioactiveDecay.java

--- Chapter-6/RadioactiveDecay.java
+++ Chapter-6/RadioactiveDecay.java
@@ -30,49 +30,5 @@ public class RadioactiveDecay extends ConsoleProgram {
 			
 		}
 		
-//		int years = 0;
-//		
-//		//display the value of private int atomsRemained at the top
-//		println("Atoms from private int atomsRemained: " + atomsRemained);
-//		//prints number of atoms at the beginning of the decay
-//		println("There are " + ATOMS_START + " atoms initially.");
-//		
-//		//prints remained atoms at the end of every year
-//		while(atomsRemained > 0){	
-		/*
-		 * this atomsRemained is working because the variable with its value is 
-		 * outside of run. And when its coming from halfLife method the while
-		 * loop doesn't start again and again with same value
-		 * and it needs a assignment with current value
-		 */
-//			years++;//counts years that takes to decay
-//			
-//			//prints how many atoms remained at the end of the years
-//			println("There are: " + halfLife(atomsRemained) + " atoms at the end of year: " + years);
-//		}
-	}
-	
-	
-	/*
-	 * this is the method that decays the atoms
-	 * in the beginning int atoms = atomsRemained = 10000
-	 * after the loop int atoms = atomsRemained returned from halfLife
-	 */
-//	private int halfLife(int atoms){
-////		int atomsRemained = atoms;
-//		for(int i = 0; i < atoms ; i++){
-//			boolean halfLife = rgen.nextBoolean();
-//			if(halfLife){
-//				atomsRemained--;
-//			}
-//		}
-//		println("Atoms from halfLife method: " + atomsRemained);
-//		return atomsRemained;
-//		
-//	}
-	
-	
-	
 	private RandomGenerator rgen = RandomGenerator.getInstance();
-	
 }
