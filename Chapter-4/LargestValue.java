9d3f424e34d77b8f357c207cadb665c1a7bc94a3
Sergiu Plesco <plescosergiu@gmail.com>
2020-02-18 15:51:35 +0000

    Update LargestValue.java

--- Chapter-4/LargestValue.java
+++ Chapter-4/LargestValue.java
@@ -1,9 +1,5 @@
 /*
  * Find largest value from a list of integers
- * ca sa afli cel mai mare numar dintr-un sir de numere se iau 2 numere care se 
- * compara intre ele. Primul numar este cel mai mare, apoi se compara cu urmatorul numar introdus.
- * daca urmatorul este mai mare decat cel mai mare, atunci cel mai mare este urmatorul.
- * Si se repeta pana este introdus 0 care indica ca loop-ul este finisat si sa arate cel mai mare numar.
  */
 import acm.program.*;
 public class LargestValue extends ConsoleProgram {
