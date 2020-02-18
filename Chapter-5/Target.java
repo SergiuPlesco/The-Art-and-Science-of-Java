fd8ada7da1db433271b1fc628331569acf8dad68
Sergiu Plesco <plescosergiu@gmail.com>
2020-02-18 15:56:03 +0000

    Update Target.java

--- Chapter-5/Target.java
+++ Chapter-5/Target.java
@@ -11,22 +11,18 @@ public class Target extends GraphicsProgram {
 		double cy = getHeight() / 2;
 		double radius = OUTER_RADIUS;
 		for(int i = N_CIRCLES; i > 0 ; i--){
-
 			add(createCircle(cx , cy, radius, (i % 2 != 0) ? Color.RED : Color.BLUE));
-			
 			radius -=16.25;
 		}
-		
-		
-		
-		
 	}
+	
 	private GOval createCircle(double x, double y, double r, Color color){
 		GOval circle = new GOval(x - r, y - r, 2 * r, 2 * r);
 		circle.setFilled(true);
 		circle.setColor(color);
 		return circle;
 	}
+	
 	private static final int N_CIRCLES = 5;
 	private static final double OUTER_RADIUS = 75;
 	private static final double INNER_RADIUS = 10;
