a0a96158f7570bbe056be68386e48e9676d98bab
Sergiu Plesco <plescosergiu@gmail.com>
2020-02-18 15:59:23 +0000

    Update MonteCarloIntegration.java

--- Chapter-6/MonteCarloIntegration.java
+++ Chapter-6/MonteCarloIntegration.java
@@ -8,9 +8,7 @@ import acm.program.*;
 import acm.util.*;
 public class MonteCarloIntegration extends ConsoleProgram {
 	public void run(){
-		
-//		println("x: " + X);
-//		println("y: " + Y);
+
 		println("throwDart: " + throwDart());
 		double dartsInCircle = 0;
 		double dartsOnlySquare = 0;
@@ -37,30 +35,6 @@ public class MonteCarloIntegration extends ConsoleProgram {
 		double throwDart = Math.pow(X, 2) + Math.pow(Y, 2);
 		return  throwDart;
 	}
+	
 	private RandomGenerator rgen = RandomGenerator.getInstance();
-//	private RandomGenerator rgen = RandomGenerator.getInstance();
-	
-	
-	/*
-	 * int i;                                                               
-   	 int nThrows = 0;                                             
-   	 int nSuccess = 0;                                            
-   									
-   	 double x, y;                                                 
-   									
-   	 for (i = 0; i < 1000000 ; i++)                         
-   	 {                                                            
-   	    x = Math.random();      // Throw a dart                   
-   	    y = Math.random();                                                
-   									
-   	    nThrows++;                                                        
-   									
-   	    if ( x*x + y*y <= 1 )             
-   	       nSuccess++;                                               
-   	 }                                                            
-   									
-   	 System.out.println("Pi/4 = " + (double)nSuccess/(double)nThrows );
-   	 System.out.println("Pi = " + 4*(double)nSuccess/(double)nThrows );
-      
-	 */
 }
