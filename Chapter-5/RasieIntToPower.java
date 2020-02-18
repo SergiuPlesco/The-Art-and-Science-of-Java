c5e208f941e055be7360e5b9d22dfb76bbe5bbd8
Sergiu Plesco <plescosergiu@gmail.com>
2020-02-18 15:55:14 +0000

    Update RasieIntToPower.java

--- Chapter-5/RasieIntToPower.java
+++ Chapter-5/RasieIntToPower.java
@@ -7,14 +7,10 @@ public class RasieIntToPower extends ConsoleProgram {
 	public void run(){
 		int n = readInt("Enter base integer: ");
 		int k = readInt("Enter power integer: ");
-//		println(n + " to the power " + k + " is: " + (int)raiseIntToPower(n, k));
 		println("The table of " + n + " to the power from 0 to " + k +" is: ");
 		raiseIntToPowerTable(n, k);
 	}
-//	private double raiseIntToPower(int n, int k){
-//		double power = Math.pow(n, k);
-//		return power;
-//	}
+
 	private void raiseIntToPowerTable(int n, int k){
 		for(int i = 0; i <= k; i++){
 			double power = Math.pow(n, i);
